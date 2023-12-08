b5aeb6741463e613604a1e99bc660360796a0203
LeHuyHung_20194579 <hung.lh194579@sis.hust.edu.vn>
2023-12-08 17:33:13 +0700

    new fix

--- src/component/Home/ListCompanies.js
+++ src/component/Home/ListCompanies.js
@@ -1,16 +1,37 @@
 import React from "react";
 import CompanyItem from "./CompanyItem";
 import PaginationPage from "./pagination/PaginationPage";
+import axios from "axios";
+import { useState } from "react";
 
 const ListCompanies = () => {
     const [arrange, setArrange] = React.useState("");
-    const [currentPage, setCurrentPage] = React.useState(0);
-    const totalPages = 10;
+    const [data, setData] = React.useState([]);
+    //const [currentPage, setCurrentPage] = React.useState(0);
+    
+    const [currentPage, setCurrentPage] = useState(1);
+    const jobPerPage = 4;
 
     const onChangeArrangeOption = (e) => {
         setArrange(e.target.value);
     };
 
+    React.useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const response = await axios.get('https://647b0017d2e5b6101db0ba04.mockapi.io/JobPostings');
+            setData(response.data);
+          } catch (error) {
+            console.error('Lỗi khi lấy dữ liệu:', error);
+          }
+        };
+        fetchData();
+      }, []);
+
+    const totalPages = Math.ceil(data.length/jobPerPage);  
+    const indexOfLastJob = (currentPage) * jobPerPage;
+    const indexOfFirstJob = indexOfLastJob - jobPerPage;
+    const currentJobs = data.slice(indexOfFirstJob, indexOfLastJob);
     return (
         <div className="w-full mt-6 mb-10">
             <div className="w-11/12 mx-auto flex flex-row justify-center items-center gap-20 text-sm">
@@ -75,10 +96,10 @@ const ListCompanies = () => {
                 </div>
             </div>
             <div className="w-11/12 mx-auto flex flex-col mt-10 gap-6">
-                <CompanyItem />
-                <CompanyItem />
-                <CompanyItem />
-                <CompanyItem />
+                {currentJobs.map((currentJob) =>(
+                    <CompanyItem companyItem={currentJob}/>
+                ))}
+                
             </div>
             <div className="w-11/12 mx-auto flex flex-row justify-center items-center ">
                 <PaginationPage
