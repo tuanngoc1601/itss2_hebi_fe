bc738aab948870b859bf3a470dd7d51d793b8bf9
NosoaB17 <ngsn176@gmail.com>
2023-12-14 17:58:33 +0700

    update JobImageContainer and DescriptionContainer

--- src/component/DetailJob/JobImageContainer.js
+++ src/component/DetailJob/JobImageContainer.js
@@ -1,45 +1,52 @@
+import React, { useState } from "react";
+
 const JobImageContainer = () => {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const imageUrls = [
+    "https://via.placeholder.com/276x184.png",
+    "https://via.placeholder.com/276x184",
+    "https://via.placeholder.com/276x184.jpg",
+  ];
+
+  const handleImageClick = (imageUrl) => {
+    // Xử lý khi click vào ảnh
+    setSelectedImage(imageUrl);
+  };
+
   return (
-    <div className="self-stretch overflow-hidden flex flex-row items-center justify-start py-0 pr-[0.000030517578125px] pl-0 gap-[4px]">
-      <div className="w-[281.77px] flex flex-col items-start justify-start py-0 pr-[5.769989013671875px] pl-0 box-border">
-        <div className="flex flex-row items-center justify-start">
-          <img
-            className="relative w-[276px] h-[184px] object-cover"
-            alt=""
-            src="/viettel026jpg@2x.png"
-          />
+    <div className="self-stretch overflow-hidden flex flex-row items-center justify-start py-0 gap-[4px]">
+      {imageUrls.map((imageUrl, index) => (
+        <div
+          key={index}
+          className="w-[281.78px] flex flex-col items-start justify-start py-0 pr-[5.779998779296875px] pl-0 box-border relative"
+        >
+          <div className="flex flex-row items-center justify-start">
+            <img
+              className="relative w-[276px] h-[184px] object-cover cursor-pointer"
+              alt={`Company Image ${index + 1}`}
+              src={imageUrl}
+              onClick={() => handleImageClick(imageUrl)}
+            />
+          </div>
+          {selectedImage === imageUrl && (
+            <>
+              <img
+                className="absolute my-0 mx-[!important] h-full w-full top-[0%] right-[0%] bottom-[0%] left-[0%] max-w-full overflow-hidden max-h-full opacity-[0.5] z-[1]"
+                alt="Overlay"
+                src="/divimgoverlay.svg"
+              />
+              <div className="my-0 mx-[!important] absolute h-[6.17%] w-[6.03%] top-[46.91%] right-[43.61%] bottom-[46.91%] left-[50.36%] flex flex-col items-start justify-start z-[2]">
+                <img
+                  className="relative w-[16.99px] h-[11.36px]"
+                  alt="Overlay Icon"
+                  src="/2.svg"
+                />
+              </div>
+            </>
+          )}
         </div>
-      </div>
-      <div className="w-[281.78px] flex flex-col items-start justify-start py-0 pr-[5.779998779296875px] pl-0 box-border">
-        <div className="flex flex-row items-center justify-start">
-          <img
-            className="relative w-[276px] h-[184px] object-cover"
-            alt=""
-            src="/viettel014jpg@2x.png"
-          />
-        </div>
-      </div>
-      <div className="w-[281.78px] flex flex-col items-start justify-start py-0 pr-[5.779998779296875px] pl-0 box-border relative">
-        <div className="flex flex-row items-center justify-start z-[0]">
-          <img
-            className="relative w-[276px] h-[184px] object-cover"
-            alt=""
-            src="/viettel033jpg@2x.png"
-          />
-        </div>
-        <img
-          className="absolute my-0 mx-[!important] h-full w-full top-[0%] right-[0%] bottom-[0%] left-[0%] max-w-full overflow-hidden max-h-full opacity-[0.5] z-[1]"
-          alt=""
-          src="/divimgoverlay.svg"
-        />
-        <div className="my-0 mx-[!important] absolute h-[6.17%] w-[6.03%] top-[46.91%] right-[43.61%] bottom-[46.91%] left-[50.36%] flex flex-col items-start justify-start z-[2]">
-          <img
-            className="relative w-[16.99px] h-[11.36px]"
-            alt=""
-            src="/2.svg"
-          />
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
